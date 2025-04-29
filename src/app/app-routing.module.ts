a44d5c34e8cba5dff4bb14d5d9bbb5662bd5158f
sagar khese <sagar.khese@studiostorti.com>
2025-04-29 13:42:26 +0530

    added link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   {
